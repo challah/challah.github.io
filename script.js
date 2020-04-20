4cf3d7b2030156411bbab153be23f0fbe30f80aa
Jordanne <yang.bread@gmail.com>
2020-04-20 16:10:05 -0400

    Changed EVERYTHING

---
+++ script.js
@@ -0,0 +1,25 @@
+(function () {
+
+  function changeTitle () {
+    let titles = [
+                  'Sister of Dini and Yuanyuan',
+                  'Digital Whimsy Advocate',
+                  'Electric Big Sister',
+                  'Cat Experience Designer',
+                  'Robot Friend'];
+
+    //document.title = titles[Math.floor(Math.random() * titles.length)];
+    document.title = 'Jordanne Chan' + ' | ' + titles[Math.floor(Math.random() * titles.length)];
+    
+  }
+
+  changeTitle();
+
+})();
+
+
+
+    function play(){
+      var audio = document.getElementById("audio");
+      audio.play();
+    }
